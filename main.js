345a4d21bf97b4191b52f8d2e434461b80b7984d
hranghlukoko@gmail.com <hranghlukoko@gmail.com>
2022-07-24 22:16:00 +0530

    tab-feature-added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContent = document.querySelectorAll('.tab-section')
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    
+    tabContent.forEach(content => {
+      content.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
