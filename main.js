5f304bceb74c2908f2b2d51ea01a05493d393b1f
hranghlukoko@gmail.com <hranghlukoko@gmail.com>
2022-07-26 09:39:34 +0530

    minor-changes

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const tabs = document.querySelectorAll('[data-tab-target]');
-const tabContent = document.querySelectorAll('.tab-section')
+const tabContents = document.querySelectorAll('.tab-section')
+
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    
-    tabContent.forEach(content => {
-      content.classList.remove('active')
-    })
     tabs.forEach(tab => {
       tab.classList.remove('active')
     })
-    
     tab.classList.add('active')
+    
+    const target = document.querySelector(tab.dataset.tabTarget)
+    
+    tabContents.forEach(content => {
+      content.classList.remove('active')
+    })
     target.classList.add('active')
   })
 })
