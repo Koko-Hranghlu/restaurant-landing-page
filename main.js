920047f8c3064be7fce2a0ee19085da26b075d2e
hranghlukoko@gmail.com <hranghlukoko@gmail.com>
2022-08-15 17:28:14 +0530

    minor-changes

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ const tabContents = document.querySelectorAll('.tab-section')
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
+    // removes any highlighted tab
     tabs.forEach(tab => {
       tab.classList.remove('active')
     })
+    // highlights the clicked tab
     tab.classList.add('active')
     
+    // gets the content id of the tab clicked 
     const target = document.querySelector(tab.dataset.tabTarget)
-    
+    // removes any tab content
     tabContents.forEach(content => {
       content.classList.remove('active')
     })
+    // shows the the current tab content
     target.classList.add('active')
   })
 })
